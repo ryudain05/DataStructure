4004fa80ad61edbad3f375019adfc5e464f8fe44
ryudain05 <dkdlsekdls@naver.com>
2024-04-08 11:15:33 +0900

    6주차: postfix 중위 - 전위 중이 - 후위 표기법 실습

--- ch6_Postfix/PostfixEval2.java
+++ ch6_Postfix/PostfixEval2.java
@@ -1,13 +1,25 @@
 package ch6_Postfix;
 
 import ch5_CirucularDoublyLinkedList.ArrayStack;
+import ch5_CirucularDoublyLinkedList.LinkedStack;
 
-public class PostfixEval {
+public class PostfixEval2 {
     public static void main(String[] args) {
         String postfix = "700 3 47 + 6 * - 4 /";
         System.out.println("Input string: " + postfix);
         int answer = evaluate(postfix);
         System.out.println("Answer: " + answer);
+
+        String infix = "(((12*5)-(6/2)/(2*4))";
+
+        postfix = infix_to_postfix(infix);
+
+        System.out.println("\n입력 중위식 = " + infix);
+        System.out.println("출력 후위식 = " + postfix);
+
+        answer = evaluate(postfix);
+        System.out.println("Answer: " + answer);
+
     }
 
     private static int evaluate(String postfix) {
@@ -61,6 +73,41 @@ public class PostfixEval {
     private static boolean isOperator(char ch) {
         return ch == '+' || ch == '-' || ch == '*' || ch == '/';
     }
-} // 코드 6-9
 
+    private static String infix_to_postfix(String infix) {
+        int infixSize = infix.length();
+        String postfix = "";
+        int j = 0;
+        LinkedStack<Character> S = new LinkedStack<>();
+
+        for (int i = 0; i < infixSize; i++) {
+            char symbol = infix.charAt(i);
+
+            switch (symbol) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    postfix = postfix + ' ';   //피연산자들 사이에 공백 추가함
+                    S.push(symbol);
+                    ;
+                    break;
+                case '(':
+                    break;
+                case ')':
+                    postfix = postfix + ' ';   //연산자들 사이에 공백 추가함
+                    postfix = postfix + S.pop();
+                    ;
+                    break;
+                default:
+                    postfix = postfix + symbol;
+            }
+        }
+        while (S.isEmpty() != true) {
+            postfix = postfix + S.pop();   //스택에 남아있는 연산자들 출력함
+        }
+
+        return postfix;
+    }
+} // 코드 6-9
 
