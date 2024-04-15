c66f7041bb4b077c5f8b4e156cd14185bc4f0203
ryudain05 <dkdlsekdls@naver.com>
2024-04-15 10:56:25 +0900

    7주차: Linked Queue Palindrom 문자열 체크 실습

---
+++ ch7_LikedQueue/LinkedQueueDemo.java
@@ -0,0 +1,34 @@
+package ch7_LikedQueue;
+
+public class LinkedQueueDemo {
+    public static void main(String[] args) {
+        // 타입 E를 String으로 결정하면서 수행하는 예
+        LinkedQueue<String> q = new LinkedQueue<>();
+        q.enqueue("test 1");
+        q.enqueue("test 2");
+        q.enqueue("test 3");
+        q.enqueue("test 4");
+        q.printAll();
+
+        System.out.println("Deleted item = " + q.dequeue());
+        System.out.println("Deleted item = " + q.dequeue());
+        q.printAll();
+
+        q.enqueue("test 6");
+        q.enqueue("test 7");
+        q.printAll();
+
+        System.out.println("Deleted item = " + q.dequeue());
+        System.out.println("Deleted item = " + q.dequeue());
+        System.out.println("Deleted item = " + q.dequeue());
+        System.out.println("Deleted item = " + q.dequeue());
+
+        if (q.isEmpty()) {
+            System.out.println("더이상 큐에 원소가 남아있지 않습니다.");
+        } else {
+            System.out.println("Deleted item = " + q.dequeue());
+            System.out.println("Deleted item = " + q.dequeue());
+            System.out.println("Deleted item = " + q.dequeue());
+        }
+    }
+}
