c2b3148a4cf0417344b0a92286a1544ae94f58af
ryudain05 <dkdlsekdls@naver.com>
2024-04-22 10:56:12 +0900

    8주차: LinkedTree 실습 과제

--- ch8_Tree/TreeDemo.java
+++ ch8_Tree/TreeDemo.java
@@ -4,14 +4,32 @@ public class TreeDemo {
 
     public static void main(String[] args) {
         LinkedTree T = new LinkedTree();
+//
+//        TreeNode n7 = T.makeBT(null, 'D', null);
+//        TreeNode n6 = T.makeBT(null, 'C', null);
+//        TreeNode n5 = T.makeBT(null, 'B', null);
+//        TreeNode n4 = T.makeBT(null, 'A', null);
+//        TreeNode n3 = T.makeBT(n6, '/', n7);
+//        TreeNode n2 = T.makeBT(n4, '*', n5);
+//        TreeNode n1 = T.makeBT(n2, '-', n3); //얘가 rootNode
+//
+//        System.out.printf("\n Preorder : ");
+//        T.preorder(n1);
+//
+//        System.out.printf("\n Inorder : ");
+//        T.inorder(n1);
+//
+//        System.out.printf("\n Postorder : ");
+//        T.postorder(n1);
 
-        TreeNode n7 = T.makeBT(null, 'D', null);
-        TreeNode n6 = T.makeBT(null, 'C', null);
-        TreeNode n5 = T.makeBT(null, 'B', null);
-        TreeNode n4 = T.makeBT(null, 'A', null);
-        TreeNode n3 = T.makeBT(n6, '/', n7);
-        TreeNode n2 = T.makeBT(n4, '*', n5);
-        TreeNode n1 = T.makeBT(n2, '-', n3); //얘가 rootNode
+        TreeNode n8 = T.makeBT(null, 'H', null);
+        TreeNode n7 = T.makeBT(null, 'G', null);
+        TreeNode n6 = T.makeBT(null, 'F', n8);
+        TreeNode n5 = T.makeBT(n7, 'E', null);
+        TreeNode n4 = T.makeBT(null, 'D', null);
+        TreeNode n3 = T.makeBT(n6, 'C', null);
+        TreeNode n2 = T.makeBT(n4, 'B', n5);
+        TreeNode n1 = T.makeBT(n2, 'A', n3); //얘가 rootNode
 
         System.out.printf("\n Preorder : ");
         T.preorder(n1);
